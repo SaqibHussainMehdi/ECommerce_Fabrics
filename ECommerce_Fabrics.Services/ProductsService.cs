3af4f2ab21e91fe54523d5fd7f1b202c90f48756
SaqibHussainMehdi <saqib_hussain36@yahoo.com>
2023-06-16 03:44:45 +0500

    Product CRUD And Search Operation Using AJAX

---
+++ ECommerce_Fabrics.Services/ProductsService.cs
@@ -0,0 +1,26 @@
+﻿using ECommerce_Fabrics.Database;
+using ECommerce_Fabrics.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce_Fabrics.Services
+{
+   
+    public class ProductsService
+    {
+        DataContext context = new DataContext();
+        public List<Product> GetProducts()
+        {
+            return context.products.ToList();
+        }
+
+        public void SaveProduct(Product product)
+        {
+            context.products.Add(product);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ ECommerce_Fabrics.Web/Controllers/ProductController.cs
@@ -0,0 +1,46 @@
+﻿using ECommerce_Fabrics.Entities;
+using ECommerce_Fabrics.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECommerce_Fabrics.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductsService productsService = new ProductsService();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Product
+        public ActionResult ProcutsTable(string search)
+        {
+           var products= productsService.GetProducts();
+            if (String.IsNullOrEmpty(search) == false)
+            {
+                products = products.Where(p =>p.Name !=null  && p.Name.ToLower().Contains(search.ToLower())).ToList();
+            }
+            
+            return PartialView(products);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            productsService.SaveProduct(product);
+            return RedirectToAction("ProcutsTable");
+        }
+    }
+}
